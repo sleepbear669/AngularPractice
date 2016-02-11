2a81a15bec1811424624fca8b8f8cce8740be010
sleepbear <sleepbear669@gmail.com>
2016-02-11 11:36:03 +0900

    Initial commit

---
+++ src/main/java/com/github/sleepbear669/Application.java
@@ -0,0 +1,14 @@
+package com.github.sleepbear669;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by sleepbear on 2016. 2. 11..
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/github/sleepbear669/controller/rootController.java
@@ -0,0 +1,16 @@
+package com.github.sleepbear669.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by sleepbear on 2016. 2. 11..
+ */
+@Controller
+public class rootController {
+
+    @RequestMapping("/")
+    public void root() {
+
+    }
+}
